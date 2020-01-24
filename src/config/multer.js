5e1a44ad63793569e6c1bf7e71c2ec211c4de17b
Mauricio Duarte <prussdev@gmail.com>
2020-01-24 18:17:14 -0300

    Add config S3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,17 +1,30 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-module.exports = {
-
-  //Local de armazenamento das imagens.
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
     },
     filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if(err) cb(err);
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'uploadexample-rocketseat',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if(err) cb(err);
 
@@ -20,7 +33,15 @@ module.exports = {
         cb(null, fileName);
       });
     }
-  }),
+  })
+}
+
+module.exports = {
+
+  //Local de armazenamento das imagens.
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+
+  storage: storageTypes.s3,
 
   //Configs de tamanho do arquivo, numero de arquivos e outros.
   limits: {
